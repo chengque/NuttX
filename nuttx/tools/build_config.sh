f400127ac096c22e3b0c76973099f237d41d9529
Lorenz Meier <lm@inf.ethz.ch>
2014-12-27 19:08:38 +0100

    Travis CI: Better config management

--- nuttx/tools/build_config.sh
+++ nuttx/tools/build_config.sh
@@ -28,13 +28,22 @@ echo "LOGDIR: $LOGDIR"
 echo "LOG: $BUILDLOG"
 mkdir -p $LOGDIR
 
+# Manually reconfigure for Linux toolchain
+sed -i 's/# CONFIG_HOST_LINUX is not set/CONFIG_HOST_LINUX=y/g' $CONFIGPATH
+sed -i 's/CONFIG_HOST_WINDOWS=y/# CONFIG_HOST_WINDOWS is not set/g' $CONFIGPATH
+sed -i 's/CONFIG_ARMV7M_TOOLCHAIN_BUILDROOT=y/# CONFIG_ARMV7M_TOOLCHAIN_BUILDROOT is not set/g' $CONFIGPATH
+sed -i 's/CONFIG_ARMV7M_TOOLCHAIN_CODEREDL=y/# CONFIG_ARMV7M_TOOLCHAIN_BUILDROOT is not set/g' $CONFIGPATH
+sed -i 's/CONFIG_ARMV7M_TOOLCHAIN_CODESOURCERYL=y/# CONFIG_ARMV7M_TOOLCHAIN_BUILDROOT is not set/g' $CONFIGPATH
+sed -i 's/# CONFIG_ARMV7M_TOOLCHAIN_GNU_EABIL is not set/CONFIG_ARMV7M_TOOLCHAIN_GNU_EABIL=y/g' $CONFIGPATH
+
 cd nuttx/tools
 ./configure.sh $CONFIG
 cd ..
 if ! (make &> $BUILDLOG); then
-	echo "CONFIG $CONFIGPATH FAILED BUILD\n\n\n"
+	echo "CONFIG $CONFIG: FAILED BUILD\n\n\n"
 	exit 1;
 else
+	echo "CONFIG $CONFIG: SUCCESSFUL BUILD\n\n\n"
 	cp nuttx $LOGDIR/.
 	cp nuttx.bin $LOGDIR/.
 	exit 0;
